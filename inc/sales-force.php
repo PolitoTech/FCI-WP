2412a75d06a74a9e6dc61af8796a4c13b6cb6d0d
Polito Technologies <info@politotech.com>
2015-07-08 15:49:18 -0400

    Update sales-force.php

--- inc/sales-force.php
+++ inc/sales-force.php
@@ -5,7 +5,7 @@
         <img src="<?php echo bloginfo( 'url' ); ?>/wp-content/uploads/2015/05/img-03.png" alt="">
         <h2 class="white">Built Natively on the Salesforce1 Platform</h2>
         <ul class="list-unstyled">
-          <li><a href="#">• Works out of the box with campaigns, leads, contacts and opportunities</a></li>
+          <li>• Works out of the box with campaigns, leads, contacts and opportunities</li>
           <li><a href="#">• All marketing and sales data is in the same system</a></li>
           <li><a href="#">• Easy to configure and seamless upgrades</a></li>
           <li><a href="#">• Compatible with the leading marketing automation systems</a></li>
