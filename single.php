7e960089340bbd4b7170b13bfbd52011a56cb4f6
kossa <hadjikouceyla@gmail.com>
2015-07-09 00:37:22 +0100

    Blog - Individual Post - Need spacing in Body fixes #9

--- single.php
+++ single.php
@@ -22,7 +22,7 @@ get_header();
           <div class="col-sm-8">
 
           <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <article>
+            <article class="single-post">
               <h1 class="title"><?php the_title(); ?></h1>
               <ul class="list-inline article-info">
                 <li class="blogger-img"><?php echo get_avatar(get_the_author_meta('ID'), 25) ?></li>
