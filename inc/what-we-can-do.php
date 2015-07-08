213067c02e7698f2a0816e11db2fa05683f05fc7
Polito Technologies <info@politotech.com>
2015-07-08 15:26:58 -0400

    Update what-we-can-do.php

--- inc/what-we-can-do.php
+++ inc/what-we-can-do.php
@@ -2,9 +2,7 @@
   <div class="container">
     <h2><span>See what Full Circle can do for you</span></h2>
       <iframe src="http://go.pardot.com/l/69472/2015-06-22/gwm4w" width="100%" height="500" type="text/html" frameborder="0" allowTransparency="true" style="border: 0"></iframe>
-   
-    <div class="text-center">
-      <img src="<?php echo bloginfo( 'url' ); ?>/wp-content/uploads/2015/05/img-05.png" alt="">
-    </div>
+   <img class ="text-center" src="<?php echo bloginfo( 'url' ); ?>/wp-content/uploads/2015/05/img-05.png" alt="">
+    
   </div>
 </section><!-- what-we-can-do -->
